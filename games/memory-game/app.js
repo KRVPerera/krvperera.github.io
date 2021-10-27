bc18e7f9fcd2c6f786305e1fd006640d0b4f039a
KRVPerera <rukshan.viduranga@gmail.com>
2021-10-27 23:13:53 +0530

    boostrap

--- games/memory-game/app.js
+++ games/memory-game/app.js
@@ -106,8 +106,10 @@ document.addEventListener('DOMContentLoaded', () => {
         var cards = document.querySelectorAll('img')
         const optionOneId = cardsChosenId[0]
         const optionTwoId = cardsChosenId[1]
-
-        if (cardsChosen[0] === cardsChosen[1]) {
+        if (optionOneId === optionTwoId) {
+            cards[optionOneId].setAttribute('src', 'images/frame.png')
+            cards[optionTwoId].setAttribute('src', 'images/frame.png')
+        } else if (cardsChosen[0] === cardsChosen[1]) {
             // alert('You found a match')
             cards[optionOneId].setAttribute('src', 'images/square.png')
             cards[optionTwoId].setAttribute('src', 'images/square.png')
